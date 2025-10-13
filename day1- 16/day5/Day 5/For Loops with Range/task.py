aed312600ca8b36d353b62f6180dbeae741591f7
Alok Kumar Yadav <timewalker126@gmail.com>
2025-10-13 22:23:04 +0530

    update

---
+++ day1- 16/day5/Day 5/For Loops with Range/task.py
@@ -0,0 +1,14 @@
+print(range(1, 10))
+
+for number in range(1, 10):
+    print(number)
+
+for number in range(1, 11):
+    print(number)
+
+
+# Gauss challenge
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
